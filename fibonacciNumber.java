3a5a94e1986dd48dc9bc83f62e3bd4aabfcf30b2
samibayat <42465611+samibayat@users.noreply.github.com>
2019-07-18 15:03:25 +0530

    Update and rename Fb.java to fibonacciNumber.java

--- fibonacciNumber.java
+++ fibonacciNumber.java
@@ -1,4 +1,4 @@
-public class Fb{  
+public class fibonacciNumber{  
     public static void main(String args[])  
     {  
    //a series of numbers in which each number ( Fibonacci number ) is   the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.  
